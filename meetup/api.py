4f3c449b70daf1e826cdc996cbff995f17b11e18
Yijin <hi@yijin.uk>
2019-08-19 15:37:59 +0100

    forgot to remove references to v1 and v2. fixed here.

--- meetup/api.py
+++ meetup/api.py
@@ -13,8 +13,6 @@ API_DEFAULT_URL = 'https://api.meetup.com/'
 TOKEN_ENV_NAME = 'MEETUP_OAUTH_TOKEN'
 API_SPEC_DIR = os.path.join(os.path.dirname(__file__), 'api_specification')
 API_SERVICE_FILES = [
-    ('v1', os.path.join(API_SPEC_DIR, 'meetup_v1_services.json')),
-    ('v2', os.path.join(API_SPEC_DIR, 'meetup_v2_services.json')),
     ('v3', os.path.join(API_SPEC_DIR, 'meetup_v3_services.json')),
 ]
 DEFAULT_WAIT_TIME = 30
